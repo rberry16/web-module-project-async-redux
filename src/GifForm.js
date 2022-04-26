fde40a8d22d9c4360596cdb13848311d1a340637
Ryan Berry <rberry16@hotmail.com>
2022-04-26 02:11:16 -0700

    all tasks completed

--- src/GifForm.js
+++ src/GifForm.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import { getGifs } from "./actions/actions";
+import { useState } from "react";
 
-const GifForm = () => {
-   return( <form>
-        <input />
+const GifForm = (props) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.getGifs(searchTerm)
+    }
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+   return( <form onSubmit={handleSubmit} >
+        <input onChange={handleChange} />
         <button>Search</button>
       </form>
    )
 }
 
-export default GifForm;
+export default connect(null, {getGifs})(GifForm);
